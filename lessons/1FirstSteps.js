023ca24954a2e8586a06a18219763da8aec384e8
ArudoPE <aortizparodi@outlook.com>
2021-04-01 09:40:27 -0500

    First commit

---
+++ lessons/1FirstSteps.js
@@ -0,0 +1,18 @@
+//1st lesson -- Variables
+//console.log, let you show something in the screen!
+//We can declare a variable using var variableName;
+//We can also declare and initialize a variable,
+// var variableName = value;
+var firstName = 'Aldo';
+var lastName = 'Ortiz';
+var age = 28;
+console.log('Hi ' + firstName + ' ' + lastName);
+console.log("I'm " + age);
+
+//2nd Lesson - Variables (Strings)
+//toUpperCase();, makes the string upper case
+var nameInUppercase = firstName.toUpperCase();
+//toLowerCasse(); makes the string lower case
+var lastNameInLowerCase = lastName.toLowerCase();
+//charAt(n); return a character of the string, "n" represents position.
+var firstLetterOfName = firstName.charAt(0);
